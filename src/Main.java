f84baae7d43655297935ffebf41e54d68e482e1f
osorov97 <osorov.nursultan97@gmail.com>
2022-09-05 16:18:26 +0600

    gsdhgf

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList <String>();
+        list.add("Alfa");
+        list.add("Delta");
+        list.add("Charli");
 
+        System.out.println("Spisok: " + list);
+
+        System.out.println("Zamenyaem: " + list.get(1) + "\n");
+        list.set (1,"Bravo");
+        list.forEach((x)->System.out.println("Element: " + x));
 
         }
 
